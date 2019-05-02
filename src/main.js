51f614a9c3f0923b62fcd7202a8c00297e96aa2f
Muhammad Aditya Hilmy <didithilmy@gmail.com>
2019-05-02 08:59:27 +0700

    Added vue analytics

--- src/main.js
+++ src/main.js
@@ -2,7 +2,13 @@ import Vue from 'vue'
 import './plugins/vuetify'
 import App from './App.vue'
 import './stylus/main.styl';
+import VueAnalytics from 'vue-analytics'
 
+if(process.env.VUE_APP_GA_ID) {
+    Vue.use(VueAnalytics, {
+        id: process.env.VUE_APP_GA_ID
+    });
+}
 
 Vue.config.productionTip = false;
 
